aeafae5c463418ee0188558541d7fae827d79f42
“espelucero” <espe.lucero@gmail.com>
2017-04-27 11:58:08 -0300

    Agregar boton de añadir tarea

---
+++ assets/javascript/main.js
@@ -0,0 +1,80 @@
+var arreglo = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 8,
+    "title": "quo adipisci enim quam ut ab",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 9,
+    "title": "molestiae perspiciatis ipsa",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 10,
+    "title": "illo est ratione doloremque quia maiores aut",
+    "completed": true
+  }];
+
+
+  function agregarTarea(){
+  	var input = document.getElementById("add-tarea");
+  	var contTarea = document.getElementsByClassName("listaTareas");
+
+  	contTarea[0].innerHTML += input.value + "<br>";
+	
+  }
+  
+
+  var item = document.createElement("li");
+
+  function listaTareas(){
+  var arr = [];
+	for (var i = 0; i < 10; i++){
+	  arr.push(res[i]);
+	}
+}
